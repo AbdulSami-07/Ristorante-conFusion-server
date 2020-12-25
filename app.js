4dc5e72f69b2e8adb09e4d980da71c3407498fe6
AbdulSami-07 <sami07israil@gmail.com>
2020-12-25 22:00:02 +0530

    File Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -61,6 +62,7 @@ app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/imageUpload',uploadRouter)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
