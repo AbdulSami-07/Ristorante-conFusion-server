65eea3ff0f3366985077727b16aad8c1aafb6958
AbdulSami-07 <sami07israil@gmail.com>
2020-12-26 19:41:07 +0530

    Assignment 4

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -60,16 +61,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/imageUpload',uploadRouter)
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/imageUpload',uploadRouter)
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
