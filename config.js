4345f073cfa85bfb65158584ba3539c3b345b403
AbdulSami-07 <sami07israil@gmail.com>
2020-12-26 19:02:32 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '107214781205519',
+        clientSecret: '90bb227f44f1210d743f7f8820889720'
+    }
 }
